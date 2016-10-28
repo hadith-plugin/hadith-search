0ab3a1288fcfe16076ff018a8eb7829d05863348
Mostafa Sameh <lambda.mostafa@gmail.com>
2016-10-28 08:23:29 +0100

    mock search api

--- app/controllers/SearchController.scala
+++ app/controllers/SearchController.scala
@@ -2,20 +2,42 @@ package controllers
 
 import akka.actor.ActorSystem
 import javax.inject._
+
 import play.api.libs.json.{JsArray, Json}
 import play.api.mvc._
+
 import scala.concurrent.ExecutionContext
+import scala.util.Try
 
 @Singleton
 class SearchController @Inject() (actorSystem: ActorSystem)(implicit exec: ExecutionContext) extends Controller {
 
-  def search = Action {
+  def getQueryAsInt[A](param: String)(implicit request: Request[A]): Option[Int] =
+    Try(request.getQueryString(param).map(Integer.parseInt)).toOption.getOrElse(None)
+
+  def search = Action { implicit request =>
+    val query = request.getQueryString("q")
+    val limit = Math.abs(getQueryAsInt("limit").getOrElse(10))
+    val offset = Math.abs(getQueryAsInt("offset").getOrElse(0))
     val json = Json.parse(
       """
-        |1
+        |{
+        |  "index": "al-bokhari",
+        |  "score": 0.9635,
+        |  "content": "   حَدَّثَنَا يَحْيَى بْنُ سُلَيْمَانَ، قَالَ   [ص: 169]  : حَدَّثَنِي ابْنُ وَهْبٍ، قَالَ: حَدَّثَنِي عُمَرُ هُوَ ابْنُ مُحَمَّدٍ، عَنْ سَالِمٍ، عَنْ أَبِيهِ، قَالَ:  وَعَدَ النَّبِيَّ صَلَّى اللهُ عَلَيْهِ وَسَلَّمَ جِبْرِيلُ، فَرَاثَ عَلَيْهِ، حَتَّى اشْتَدَّ عَلَى النَّبِيِّ صَلَّى اللهُ عَلَيْهِ وَسَلَّمَ، فَخَرَجَ النَّبِيُّ صَلَّى اللهُ عَلَيْهِ وَسَلَّمَ فَلَقِيَهُ، فَشَكَا إِلَيْهِ مَا وَجَدَ، فَقَالَ لَهُ: إِنَّا لاَ نَدْخُلُ بَيْتًا فِيهِ صُورَةٌ وَلاَ كَلْبٌ    [تعليق مصطفى البغا]  5615 (5/2222) -[  ش (فراث) أبطأ في النزول. (اشتد) ثقل عليه تأخر نزوله وأحزنه ذلك] [ر 3055] ",
+        |  "noTashkeelContent": "   حدثنا يحيى بن سليمان، قال   [ص: 169]  : حدثني ابن وهب، قال: حدثني عمر هو ابن محمد، عن سالم، عن أبيه، قال:  وعد النبي صلى الله عليه وسلم جبريل، فراث عليه، حتى اشتد على النبي صلى الله عليه وسلم، فخرج النبي صلى الله عليه وسلم فلقيه، فشكا إليه ما وجد، فقال له: إنا لا ندخل بيتا فيه صورة ولا كلب    [تعليق مصطفى البغا]  5615 (5/2222) -[  ش (فراث) أبطأ في النزول. (اشتد) ثقل عليه تأخر نزوله وأحزنه ذلك] [ر 3055] ",
+        |  "book": {
+        |    "content": "« وَكُنْتُ أَغْتَسِلُ أَنَا وَالنَّبِيُّ صَلَّى اللهُ عَلَيْهِ وَسَلَّمَ مِنْ إِنَاءٍ وَاحِدٍ»",
+        |    "noTashkeelContent": "« وكنت أغتسل أنا والنبي صلى الله عليه وسلم من إناء واحد»"
+        |  },
+        |  "chapter": {
+        |    "content": " بَابُ لاَ تَدْخُلُ المَلاَئِكَةُ بَيْتًا فِيهِ صُورَةٌ ",
+        |    "noTashkeelContent": " باب لا تدخل الملائكة بيتا فيه صورة "
+        |  }
+        |}
       """.stripMargin)
 
-    Ok(JsArray(Seq.fill(10)(json)))
+    Ok(JsArray(Seq.fill(limit)(json)))
   }
 
 
